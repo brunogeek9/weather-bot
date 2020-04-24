6b994302bf207b7b2dba7734c8dc16074ee8b708
brunogeek9 <brunogeek9@gmail.com>
2020-04-24 19:40:35 -0300

    formatando a temperatura para graus celcius

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ const weatherToken = process.env.WEATHER_API_TOKEN;
 const fetch = require('node-fetch');
 
 const weatherUrl = new URL('https://api.openweathermap.org/data/2.5/weather');
-// weatherUrl.searchParams.set('zip', '78747,us');
 weatherUrl.searchParams.set('q', 'London,uk');
-
 weatherUrl.searchParams.set('APPID', weatherToken);
-
-// weatherUrl.searchParams.set('units', '');
 
 const getWeatherData = async() => {
     const resp = await fetch(weatherUrl.toString());
@@ -16,11 +12,20 @@ const getWeatherData = async() => {
     return body;
 }
 
+const generateWeatherMessage = weatherData =>
+    `The Weather data in ${weatherData.name}: ${weatherData.weather[0].description}. Current\
+ Temperature is ${weatherData.main.temp}, with a low temp of ${weatherData.main.temp_min}\
+ and high of ${weatherData.main.temp_max}.`
+
+const convertCelcius = data => (data.main.temp - 273.15).toFixed(2)
+
 const main = async() => {
     const weatherData = await getWeatherData();
-    console.log(weatherData);
-    console.log((weatherData.main.temp - 273, 15));
-
+    const weatherString = generateWeatherMessage(weatherData);
+    // console.log(weatherData);
+    console.log(`Celcius Temp ${convertCelcius(weatherData)}`);
+    console.log(weatherString);
 }
+
 
 main();
